6ce801ad398a0afe01c2d4bd8cd2007276403d21
kevin <kevin418038748@sina.cn>
2013-05-02 17:33:06 +0800

    kevin

---
+++ src/main/java/cn/edu/ccdx/ccdx_util/App.java
@@ -0,0 +1,13 @@
+package cn.edu.ccdx.ccdx_util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
